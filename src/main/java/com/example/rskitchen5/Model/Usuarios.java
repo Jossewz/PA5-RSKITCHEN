df3db041c50e4baf4681fdf02894d3a6ccb126c4
Jossewz <joseayolaar@gmail.com>
2025-04-15 17:57:48 -0500

    Model add

--- src/main/java/com/example/rskitchen5/Model/Usuarios.java
+++ src/main/java/com/example/rskitchen5/Model/Usuarios.java
@@ -1,4 +1,12 @@
 package com.example.rskitchen5.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuarios {
+
+    @Id
+    private String id;
+
 }
