891ad84a9fb9fed56df773b2f5bc6ca180bafbde
JesusC33 <dcaldera2005@gmail.com>
2025-05-16 18:41:58 -0500

    te odio mesero 5

--- src/main/java/com/example/rskitchen5/Model/Mesero.java
+++ src/main/java/com/example/rskitchen5/Model/Mesero.java
@@ -3,7 +3,7 @@ package com.example.rskitchen5.Model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "usuarios")
+@Document(collection = "meseros")
 public class Mesero {
 
     @Id
--- src/main/java/com/example/rskitchen5/Repository/MeseroRep.java
+++ src/main/java/com/example/rskitchen5/Repository/MeseroRep.java
@@ -1,7 +1,6 @@
 package com.example.rskitchen5.Repository;
 
 import com.example.rskitchen5.Model.Mesero;
-import com.example.rskitchen5.Model.Producto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
